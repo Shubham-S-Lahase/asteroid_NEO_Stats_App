01f09c1dd7a4de1fbedbfd772e10f3a4543d29a1
Shubham Lahase <shubhamlahase@gmail.com>
2024-09-08 10:21:49 +0530

    feat: add DatePicker component for selecting start and end dates

---
+++ components/DatePicker.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="date" :value="selectedDate" @input="updateDate" />
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const selectedDate = ref('');
+const emit = defineEmits(['update:date']);
+
+const updateDate = (event) => {
+  selectedDate.value = event.target.value; // Update the local state
+  emit('update:date', selectedDate.value); // Emit the change
+};
+</script>
