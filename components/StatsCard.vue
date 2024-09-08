222f926f22a0995b60b8af1caccb69ca2107e8c8
Shubham Lahase <shubhamlahase@gmail.com>
2024-09-08 10:23:10 +0530

    feat: create StatsCard component to display asteroid statistics

---
+++ components/StatsCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-6 bg-white shadow-lg rounded-lg hover:shadow-xl transition-shadow">
+    <h2 class="text-2xl font-semibold text-gray-800 mb-2">{{ title }}</h2>
+    <p class="text-lg text-gray-700">
+      {{ valueLabel }}:
+      <span v-if="typeof value === 'object'">
+        <a :href="value.url" target="_blank" class="text-blue-600 underline">{{ value.name }}</a>
+      </span>
+      <span v-else>
+        {{ value || "N/A" }}
+      </span>
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  value: [Object, String, Number],
+});
+
+const valueLabel = computed(() => {
+  if (props.title === "Fastest Asteroid") return "Speed";
+  if (props.title === "Closest Asteroid") return "Distance";
+  if (props.title === "Average Size") return "Average Diameter";
+  return "";
+});
+</script>
