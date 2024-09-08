ff2df992f6a93a856e51f4af7ffc9df9739125b1
Shubham Lahase <shubhamlahase@gmail.com>
2024-09-08 09:45:48 +0530

    added nuxt echart and tailwindcss and configured them

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    'nuxt-echarts',
+  ],
+  echarts: {
+    charts: ['LinesChart'],
+    components: ['TitleComponent', 'TooltipComponent', 'GridComponent']
+  },
+  runtimeConfig: {
+    public: {
+      nasaApiKey: process.env.NASA_API_KEY || 'DEMO_KEY',
+    },
+  },
+  css: ['~/assets/css/tailwind.css'],
 })
